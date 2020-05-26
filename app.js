b095169628578df971b2995b43edbcaa3e0dd23f
nbrooks89 <nbrooks2389@gmail.com>
2020-05-26 00:27:44 -0700

    about

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
 }); 
+
+
+
+const myFunction = () => {
+    document.getElementById("first").style.display ='block';
+    document.getElementById("second").style.display ='none'
+    document.getElementById("third").style.display ='none'
+  }
+  
+  const myFunction2 = () => {
+    document.getElementById("second").style.display ='block'
+    document.getElementById("first").style.display ='none'
+    document.getElementById("third").style.display ='none'
+  }
+  
+  const myFunction3 = () => {
+    document.getElementById("third").style.display ='block'
+    document.getElementById("first").style.display ='none'
+    document.getElementById("second").style.display ='none'
+  }
